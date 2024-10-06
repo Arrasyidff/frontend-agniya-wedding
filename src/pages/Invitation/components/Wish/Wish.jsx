be4abbb5f8b31b189be2589fcb843ec03dfda75b
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-10-06 13:11:48 +0800

    fix: fix handle wishes ui

--- src/pages/Invitation/components/Wish/Wish.jsx
+++ src/pages/Invitation/components/Wish/Wish.jsx
@@ -7,11 +7,30 @@ function Wish() {
     }
     return (
         <section className='ai-wish__container'>
-            <h1>DOA DAN UCAPAN</h1>
-            <form onSubmit={handleOnSubmit}>
-                <textarea placeholder='Tulis Harapan Kamu' name="wish"></textarea>
-                <button type='submit'>Kirim</button>
-            </form>
+            <div className='ai-wish__form'>
+                <h1>DOA DAN UCAPAN</h1>
+                <form onSubmit={handleOnSubmit}>
+                    <textarea placeholder='Tulis Harapan Kamu' name="wish"></textarea>
+                    <button type='submit'>Kirim</button>
+                </form>
+            </div>
+
+            <div className='ai-wish__wishes'>
+                {
+                    ([1,2,3,4,5]).map(item => (
+                        <div key={item} className='ai-wish__wishes-item'>
+                            <div className='ai-wish__wishes-item__initial'>
+                                <h1>E</h1>
+                            </div>
+                            <div className='ai-wish__wishes-item__detail'>
+                                <p>Eddy Radja</p>
+                                <p>Selamat menjalani kehidupan baru yang penuh bahagia. Semoga Tuhan selalu melindungi dan memberikan berkah. Amin.</p>
+                                <p>1 minggu yang lalu at 18:26</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </section>
     )
 }
