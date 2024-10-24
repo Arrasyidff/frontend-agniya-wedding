d5981cd92859ed5aef785af17c055b457e5e979a
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-10-25 06:15:44 +0800

    feat: handle navigation bar

--- src/pages/Invitation/Invitation.jsx
+++ src/pages/Invitation/Invitation.jsx
@@ -1,7 +1,7 @@
 import './invitation.scss'
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Cover, Quote, Brides, Detail, Gallery, Rsvp, Wish, Gift } from './components'
+import { Cover, Quote, Brides, Detail, Gallery, Rsvp, Wish, Gift, Navigation } from './components'
 import { getInvitation } from '@store/actions/invitation'
 import { useParams } from 'react-router-dom'
 
@@ -26,6 +26,7 @@ function Invitation() {
             <Rsvp invitation={ invitation } />
             <Wish invitation={ invitation } />
             <Gift />
+            <Navigation />
         </div>
     )
 }
---
+++ src/pages/Invitation/components/Navigation/Navigation.jsx
@@ -0,0 +1,46 @@
+import './navigation.scss'
+import qrQode from '@assets/qr_code.png'
+import quoteNav from '@assets/nav_quote.png'
+import brideNav from '@assets/nav_bride.png'
+import dateNav from '@assets/nav_date.png'
+import galleryNav from '@assets/nav_gallery.png'
+import wishNav from '@assets/nav_wish.png'
+import giftNav from '@assets/nav_gift.png'
+
+function Navigation() {
+    return (
+        <div className='ai-navigation__container'>
+            <div className='ai-navigation__sub-container'>
+                <div className='ai-navigation__left-side'>
+                    <div className='ai-navigation__icon'>
+                        <img src={quoteNav} alt="" />
+                    </div>
+                    <div className='ai-navigation__icon'>
+                        <img src={brideNav} alt="" />
+                    </div>
+                    <div className='ai-navigation__icon'>
+                        <img src={dateNav} alt="" />
+                    </div>
+                </div>
+                <div className='ai-navigation__right-side'>
+                    <div className='ai-navigation__icon'>
+                        <img src={galleryNav} alt="" />
+                    </div>
+                    <div className='ai-navigation__icon'>
+                        <img src={wishNav} alt="" />
+                    </div>
+                    <div className='ai-navigation__icon'>
+                        <img src={giftNav} alt="" />
+                    </div>
+                </div>
+                <div className='ai-navigation__qr-code'>
+                    <div className='ai-navigation__qr-code__sub'>
+                        <img src={qrQode} alt="" srcset="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
