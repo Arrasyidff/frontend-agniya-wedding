f6f37642840ebcd3ac22c6dd5abc8f0d8cd5fbb8
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-10-27 15:38:51 +0800

    fix: fix brides ui

--- src/pages/Invitation/components/Brides/Brides.jsx
+++ src/pages/Invitation/components/Brides/Brides.jsx
@@ -1,41 +1,53 @@
 import './brides.scss'
 import brideFlower from '@assets/brides_flower.png'
-import backgroundGradient from '@assets/background_gradient.png'
+// import backgroundGradient from '@assets/background_gradient.png'
 
 function Brides() {
   return (
     <section id='ai-brides' className='ai-brides__container'>
-        <img className='ai-brides--background-gradient' src={backgroundGradient} alt="" srcSet="" />
+        {/* <img className='ai-brides--background-gradient' src={backgroundGradient} alt="" srcSet="" /> */}
         <div className='ai-brides__bride-container'>
-            <div className='ai-brides__bride-image'>
-            </div>
+            <div className='ai-brides__bride-image'></div>
             <div className='ai-brides__bride-detail'>
-                <h1 className='ai-brides__bride-detail--nickname'>AGHNIYA</h1>
-                <span className='ai-brides__bride-detail--separator'></span>
-                <h1 className='ai-brides__bride-detail--fullname'>
-                    AGHNIYARRIZQI IARA FADHILLA
-                </h1>
-                <span className='ai-brides__bride-detail--from'>PUTRI DARI :</span>
-                <h1 className='ai-brides__bride-detail--parent-name'>BPK. H. MUKHLIS WAHYUDIN</h1>
-                <span className='ai-brides__bride-detail--parent-separator'>&</span>
-                <h1 className='ai-brides__bride-detail--parent-name'>IBU. SRI UTAMI</h1>
+                <div className='ai-brides__bride-detail__nickname'>
+                    <h1>Aghniya</h1>
+                    <span></span>
+                </div>
+                <div className='ai-brides__bride-detail__info'>
+                    <h1 className='ai-brides__bride-detail__info--name'>
+                        Aghniyarrizqi Iara Fadhilla
+                    </h1>
+                    <p>Son of</p>
+                    <h1 className='ai-brides__bride-detail__info--name'>
+                        Bpk. H. Mukhlis Wahyudin
+                    </h1>
+                    <h1 className='ai-brides__bride-detail__info--name'>
+                        Ibu. Sri Utami
+                    </h1>
+                </div>
             </div>
         </div>
         <img className='ai-brides--bride-flower' src={brideFlower} alt="" />
         <div className='ai-brides__bride-container'>
             <div className='ai-brides__bride-detail'>
-                <h1 className='ai-brides__bride-detail--nickname'>IZZUL</h1>
-                <span className='ai-brides__bride-detail--separator'></span>
-                <h1 className='ai-brides__bride-detail--fullname'>
-                    MUHAMMAD DZULFIQAR NOOR
-                </h1>
-                <span className='ai-brides__bride-detail--from'>PUTRA DARI :</span>
-                <h1 className='ai-brides__bride-detail--parent-name'>BPK. RUDY WIDYATMIKO</h1>
-                <span className='ai-brides__bride-detail--parent-separator'>&</span>
-                <h1 className='ai-brides__bride-detail--parent-name'>IBU. SITI NURHAYATI</h1>
+                <div className='ai-brides__bride-detail__nickname'>
+                    <h1>Izzul</h1>
+                    <span></span>
+                </div>
+                <div className='ai-brides__bride-detail__info'>
+                    <h1 className='ai-brides__bride-detail__info--name'>
+                        Muhammad Dzulfiqar Noor
+                    </h1>
+                    <p>Son of</p>
+                    <h1 className='ai-brides__bride-detail__info--name'>
+                        Bpk. Rudy Widyatmiko
+                    </h1>
+                    <h1 className='ai-brides__bride-detail__info--name'>
+                        Ibu. Siti Nurhayati
+                    </h1>
+                </div>
             </div>
-            <div className='ai-brides__bride-image'>
-            </div>
+            <div className='ai-brides__bride-image'></div>
         </div>
     </section>
   )
