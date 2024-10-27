20a68b2e02e432d4d940ea67749925f91fff98f6
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2024-10-27 17:22:38 +0800

    fix: fix wish, rsvp ui

--- src/pages/Invitation/components/Gallery/Gallery.jsx
+++ src/pages/Invitation/components/Gallery/Gallery.jsx
@@ -4,7 +4,7 @@ import backgroundGradient from '@assets/background_gradient.png'
 function Gallery() {
     return (
         <section id='ai-gallery' className='ai-gallery__container'>
-            <img className='ai-gallery--background-gradient' src={backgroundGradient} alt="" srcSet="" />
+            {/* <img className='ai-gallery--background-gradient' src={backgroundGradient} alt="" srcSet="" /> */}
             <div className="ai-gallery__content">
                 <div className='ai-gallery__title'>
                     <h1>GALLERY</h1>
